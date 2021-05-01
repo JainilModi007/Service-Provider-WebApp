4464f431fb85e12eab1fe7c93510d2b858179158
JainilModi007 <jackmodi08@gmail.com>
2021-05-01 12:35:46 +0530

    Service Provider Web

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Service-Provider-WebApp
+step 1 : install visual studio code
+step 2 : create account on mongodb atlas and replace URL in mongodb.connect (in server.js file)
+step 3 : install node-modules in root directory
+         Command : node install node-modules
+
+         install node-modules in service-backend folder
+         Command : node install node-modules
+
+step 4 : run server.js file in command promt (/src/service-backend/server.js) (keep open mongodb atlas in  background)
+         Command : node server.js
+
+step 5 : run react in visual studio terminal
+         Command : npm start
---
+++ src/service-backend/models/dbshopkeeperdetails.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const shopkeeperDetailsSchema = new mongoose.Schema({
+    shopName: {
+        type : String,
+        required : true,
+    } ,
+
+    shopkeeperName : {
+        type : String,
+        required : true ,
+    },
+
+    shopkeeperID: {
+        type : Number,
+        required : true,
+        unique : true 
+        
+    },
+
+    emailID : {
+        type : String ,
+        require : true,
+        unique : true
+    }
+
+    
+})
+
+const ShopkeeperDetails = new mongoose.model("ShopkeeperDetails" , shopkeeperDetailsSchema);
+module.exports = ShopkeeperDetails ;
